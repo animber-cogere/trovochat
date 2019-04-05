ca0b0a919f2e3e001d7067b3b8577085799cd00d
animber-coder <176597279+animber-coder@users.noreply.github.com>
2019-04-05 14:42:54 -0400

    the Read portion of the Client is now swappable
    added a few traits to facilitate the swapping of the read-half
    of the client
    
    ReadAdapter replaces the bare std::io::Read impl with something that
    produces the messages + a way to hand off a writer + a way to get the
    inner reader out of the client
    
    ToMessage allows external IRC-style messages to be used with the Twitch
    Commands

--- src/twitch/extension.rs
+++ src/twitch/extension.rs
@@ -13,9 +13,10 @@ pub trait WriterExt {
     /// amount of actual writes as possible
     ///
     /// ```no_run
-    /// # use trovochat::{helpers::TestStream, Client};
+    /// # use trovochat::{helpers::TestStream, Client, SyncReadAdapter};
     /// # let mut stream = TestStream::new();
     /// # let (r, w) = (stream.clone(), stream.clone());
+    /// # let r = SyncReadAdapter::new(r);
     /// # let mut client = Client::new(r, w);
     /// use trovochat::WriterExt as _;
     /// let writer = client.writer();
