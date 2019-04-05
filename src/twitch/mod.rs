40b0078daf73b78f8499faf3e257ac464cce4176
animber-coder <176597279+animber-coder@users.noreply.github.com>
2019-04-05 17:47:17 +0200

    Add badge-info
    Twitch has to say this about the tag
    > Metadata related to the chat badges in the badges tag.
    >
    > Currently this is used only for subscriber, to indicate the exact number of months the user has been a subscriber. This number is finer grained than the version number in badges. For example, a user who has been a subscriber for 45 months would have a badge-info value of 45 but might have a badges version number for only 3 years.
    
    Example: `@badge-info=subscriber/13;badges=subscriber/12;color=#0000FF;display-name=emilgardis;emote-sets=0;mod=0;subscriber=1;user-type= :tmi.twitch.tv USERSTATE #jtv`

--- src/twitch/mod.rs
+++ src/twitch/mod.rs
@@ -4,7 +4,7 @@ mod emotes;
 
 use std::sync::atomic::{AtomicUsize, Ordering};
 
-pub use self::badge::{Badge, BadgeKind};
+pub use self::badge::{Badge, BadgeInfo, BadgeKind};
 pub use self::emotes::Emotes;
 
 pub use self::color::{trovo_colors as colors, Color, TrovoColor, RGB};
