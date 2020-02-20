a809d040b52b33df074a25db8cd4883a9214293e
animber-coder <176597279+animber-coder@users.noreply.github.com>
2020-02-20 14:00:58 -0500

    ensure client runs, shows a demo of blocking

---
+++ examples/wait_to_join.rs
@@ -0,0 +1,45 @@
+/* in your Cargo.toml
+[dependencies]
+trovochat = "0.8.3"                             # this crate
+tokio = { version = "0.2", features = ["full"] } # you need tokio to run it
+*/
+
+use tokio::stream::StreamExt as _; // for .next()
+
+#[tokio::main]
+async fn main() {
+    let (nick, pass, channel) = (
+        std::env::var("TROVO_NICK").unwrap(),
+        std::env::var("TROVO_PASS").unwrap(),
+        std::env::var("TROVO_CHANNEL").unwrap(),
+    );
+
+    let tls = trovochat::Secure::UseTls;
+    let (read, write) = trovochat::connect_easy(&nick, &pass, tls).await.unwrap();
+
+    let client = trovochat::Client::new();
+    // this runs the client in a background task, giving a future you wait on
+    // you should call run before you 'block'
+    let done = client.run(read, write);
+
+    // subscribe an Irc Ready event
+    // GlobalUserState can also be used to 'wait' for ready
+    let mut ready = client
+        .dispatcher()
+        .await
+        .subscribe::<trovochat::events::IrcReady>();
+
+    // 'block' until we've received an IrcReady event
+    let _ready = ready.next().await.unwrap();
+    // its safe to join channels after this point
+
+    // join a channel
+    client.writer().join(channel).await.unwrap();
+
+    use trovochat::client::Status;
+    match done.await {
+        Ok(Status::Eof) => eprintln!("done!"),
+        Ok(Status::Canceled) => eprintln!("client was stopped by user"),
+        Err(err) => eprintln!("error: {}", err),
+    }
+}
