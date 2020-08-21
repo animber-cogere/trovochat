ae8297748296805ebd37d4c6da79518fbb64954f
animber-coder <176597279+animber-coder@users.noreply.github.com>
2020-08-20 22:19:50 -0400

    fix doc tests

--- src/irc.rs
+++ src/irc.rs
@@ -12,11 +12,11 @@
 //! With the parsed type, you can further refine it into specific Trovo-oriented messages.
 //!
 //! ```
-//! use trovochat::{IrcMessage, InvalidMessage};
+//! use trovochat::{IrcMessage, MessageError};
 //! // a raw message from the server
 //! let input = "@key1=val;key2=true :user!user@user PRIVMSG #some_channel :\x01ACTION hello world\x01\r\n";
 //!
-//! type MsgResult<'a> = Result<IrcMessage<'a>, InvalidMessage>;
+//! type MsgResult<'a> = Result<IrcMessage<'a>, MessageError>;
 //! // this'll return an iterator over any messages in the `input` string.
 //! let mut messages: Vec<MsgResult<'_>> = trovochat::irc::parse(input).collect();
 //! // we should have only gotten 1 message
